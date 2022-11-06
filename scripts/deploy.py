0d2a6bd2368f45858bf02b6345ac44a48efd33c8
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-06 21:05:18 +0530

    deploy sol using brownie

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,15 @@
-from brownie import accounts
+from brownie import accounts,config,SimpleStorage 
 # import brownie
 def deploy_simple_storage():
-    account = accounts.load("eth-project")
-    print(account)
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from" : account})
+    stored_value = simple_storage.get_fav_no()
+    print(stored_value)  
+    transaction = simple_storage.set_fav_no(15,{"from":account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.get_fav_no()
+    print(updated_stored_value)
+
 
 def main():
     deploy_simple_storage()
