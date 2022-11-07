d57dcb6f83890b887b1bb8d07f9fe0744cb85a15
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-08 00:01:00 +0530

    deploy_fund_me function written

---
+++ scripts/deploy_fund_me.py
@@ -0,0 +1,9 @@
+from brownie import FundMe
+from scripts.deploy import get_account
+def deploy_fund_me():
+    account = get_account()
+    fund_me = FundMe.deploy({"from":account},publish_source = True)
+    print(f"Contract Deployed to {fund_me.address}")
+
+def main():
+    deploy_fund_me()
