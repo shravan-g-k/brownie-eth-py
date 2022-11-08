742aa723c31b28501725347586c2d338d268eb07
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-08 23:28:55 +0530

    deploy_fund_me completed

--- scripts/deploy_fund_me.py
+++ scripts/deploy_fund_me.py
@@ -1,4 +1,4 @@
-from brownie import FundMe, network, config
+from brownie import FundMe, network, config, MockV3Aggregator
 from scripts.deploy import get_account
 
 
@@ -6,15 +6,21 @@ def deploy_fund_me():
     # get the account from local ganache or goerli network
     account = get_account()
     if network.show_active() != "development":
-        price_feed_address = config["network"][network.show_active()][
+        price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
-    
+    else:
+        print(f"We are on network {network.show_active()}")
+        print("Deploying Mocks")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 20000000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
 
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},
-        publish_source=True,
+        publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract Deployed to {fund_me.address}")
 
