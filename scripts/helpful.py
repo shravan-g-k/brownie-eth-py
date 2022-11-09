3c84bc452a3f272e21269e79e9ee0edab3e420f4
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-09 23:12:01 +0530

    added some buggy code

--- scripts/helpful.py
+++ scripts/helpful.py
@@ -1,12 +1,13 @@
 from brownie import network, accounts, config, MockV3Aggregator
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKACHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 DECIMALS = 8
 STARTING_VALUE = 20000000000
 
 def get_account():
-    if network.show_active()in LOCAL_BLOCKACHAIN_ENVIRONMENTS:
+    if network.show_active() in LOCAL_BLOCKACHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["key"])
