21e3afcef69b94b67f653ab1029597f944bc78b6
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-13 22:38:15 +0530

    some basic code added

---
+++ ERC_20/contracts/token.sol
@@ -0,0 +1,11 @@
+// contracts/token.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Token", "T") {
+        _mint(msg.sender, initialSupply);
+    }
+}
